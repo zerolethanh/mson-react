eebdc4805c7353c6be7ba6c984b8054c7fb20139
lvt <thanh@classfunc.com>
2020-04-21 14:36:18 +0700

    name

--- src/demo/app.js
+++ src/demo/app.js
@@ -21,12 +21,12 @@ compiler.registerComponent('FormBuilder', FormBuilder);
 // Register all the components
 for (let name in components) {
   let component = components[name];
-  compiler.registerComponent(component.name, component);
+  compiler.registerComponent(name, component);
 }
 
 // Instantiate the app
 const app = compiler.newComponent({
-  component: 'app.App'
+  component: 'app.App',
 });
 
 export default app;
